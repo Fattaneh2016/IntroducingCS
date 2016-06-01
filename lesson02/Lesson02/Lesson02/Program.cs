5c255c2ceaafc2d1f4454076d5455aa4dc6d0792
Fattaneh2016 <fatameri@hotmail.com>
2016-06-01 14:49:15 -0400

    c sharp

---
+++ lesson02/Lesson02/Lesson02/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Rectangle myrect = new Rectangle(10.5, 20.6);
+
+            //double area = myrect.GetArea();
+            //Console.WriteLine("this is my rectangle area:{0}", area);
+
+            //Rectangle myrec = new Rectangle();
+            //myrec.Length = 10.5;
+            //myrec.Width = 4.2;
+            //double area = myrec.GetArea();
+            //Console.WriteLine("this is my rectangle area  {0}", area);
+
+            //Auto-implemented properties simplify property declarations 
+
+            //public double Width { get; set; }
+            //public double Length { get; set; }
+
+            //Rectangle myrec = new Rectangle { Length = 20.5, Width = 5.9 };
+
+            //Console.WriteLine("this is my area {0}" , myrec.GetArea());
+
+            //}
+
+
+            Rectangle r = new Rectangle();
+
+            r.Changed += new EventHandler(r_Changed);
+            r.Length = 10.2;
+            Console.WriteLine("this is {0}" , Rectangle.shapeName );
+
+        }
+        static void r_Changed(object sender, EventArgs e)
+    
+            {
+                Rectangle r = (Rectangle)sender;
+                Console.WriteLine("Value Changed: Length = {0}", r.Length);
+            }
+
+    }
+
+
+ }
+
---
+++ lesson02/Lesson02/Lesson02/Rectangle.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson02
+{
+    class Rectangle
+    {
+        //private double length ;
+        //private double width;
+
+        //public Rectangle(double l , double w)
+        //{
+
+        //    length = l;
+        //    width = w;
+        //}
+        //public double GetArea()
+        //{
+        //    return length * width;
+
+        //}
+
+
+        public event EventHandler Changed;
+        private double width;
+        private double length;
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                if (value > 0.0)
+                    width = value;
+            }
+        }
+        public double Length
+        {
+            get
+            {
+                return length;
+            }
+            set
+            {
+                if (value > 0.0)
+                    length = value;
+                Changed(this, EventArgs.Empty);
+            }
+        }
+
+        public double GetArea()
+        {
+            return width * length;
+        }
+
+        public static string shapeName
+        {
+            get { return "Rectangle"; }
+        }
+    }
+}
